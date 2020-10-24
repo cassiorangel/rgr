3c6ad8f8cb57d232b640ba604d94911313c8f93c
Cássio Fernandes <cassio.rfernandes@gmail.com>
2020-10-23 21:06:01 -0300

    Rotas configuradas

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailUsersComponent } from './detail-users/detail-users.component';
+import { ListUsersComponent } from './list-users/list-users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListUsersComponent
+  },
+  {
+    path: 'detalhe-user',
+    component: DetailUsersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
